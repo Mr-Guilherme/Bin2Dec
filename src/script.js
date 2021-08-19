541923b4ec17a5f57dcc55899a046cdb63cd051f
Guilherme Rodrigues <guilherme.dev2019@gmail.com>
2021-08-19 13:27:05 +0000

    Create basic Html structure, simple stylesheet and logic in Js to convert bin to dec

---
+++ src/script.js
@@ -0,0 +1,67 @@
+const $inputBinary = document.querySelector("#input-binary-number");
+const $inputDecimal = document.querySelector("#input-decimal-number");
+
+const $outputBinary = $inputDecimal;
+const $outputDecimal = $inputBinary;
+
+const Dom = {
+    renderDec(decimal) {
+        $outputBinary.classList.remove("error");
+        $outputBinary.placeholder = "Insert your decimal number";
+
+        $outputBinary.value = decimal;
+    },
+
+    // renderBin(binary) {
+    //     $inputBinary.classList.remove("error");
+    //     $inputBinary.placeholder = "Insert your binary number";
+
+    //     $inputBinary.value = binary;
+    // },
+
+    errorBin() {
+        $outputBinary.classList.add("error");
+        $outputBinary.placeholder = "Insert a binary number valid (0 or 1)";
+
+        $outputBinary.value = "";
+    }
+}
+
+function handleValue(e) {
+    e.preventDefault();
+    const inputBinary = String($inputBinary.value);
+
+    if(inputBinary == "") {
+        $outputBinary.value = "";
+        return;
+    }
+
+    const binaryString = inputBinary.replace(/\s/g, ""); // Remove white spaces
+    
+    main(binaryString);
+}
+
+function convertBinToDec(binary) {
+    const decimal = parseInt(binary, 2);
+    return decimal;
+}
+
+// function convertDecToBin(decimal) {
+//     const decimalNum = Number(decimal);
+//     const binary = (decimalNum >>> 0).toString(2);
+//     return binary;
+// }
+
+function main(inputValue) {
+    const regexOnlyBin = !(/[^0-1]/g.test(inputValue));
+
+    if(regexOnlyBin) {
+        const decimal = convertBinToDec(inputValue)
+        Dom.renderDec(decimal)
+    } else {
+        Dom.errorBin();
+    }
+   
+}
+
+$inputBinary.addEventListener('keyup', handleValue);
